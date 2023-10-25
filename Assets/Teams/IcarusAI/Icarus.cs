68bac104766164bdc14371c2a3c7fa2562e368ad
Cocomineral <tintinch59@gmail.com>
2023-10-25 10:00:33 +0200

    [ADD] Setup Icarus blackboard variables necessary to maneuver the spaceship

--- Assets/Teams/IcarusAI/Icarus.cs
+++ Assets/Teams/IcarusAI/Icarus.cs
@@ -17,19 +17,20 @@ namespace Icarus
 		{
             base.Initialize(spaceship, data);
             _otherSpaceship = data.GetSpaceShipForOwner(1 - spaceship.Owner);
+            _b.SetVariableValue("orientation", spaceship.Orientation);
         }
 
         public override InputData UpdateInput(SpaceShipView spaceship, GameData data)
 		{
-            //float thrust = 1.0f;
             //float targetOrient = spaceship.Orientation + 90.0f;
             //bool needShoot = AimingHelpers.CanHit(spaceship, otherSpaceship.Position, otherSpaceship.Velocity, 0.15f);
-            //return new InputData(thrust, targetOrient, needShoot, false, false);
-            SharedVariable a = _b.GetVariable("thrust");
-            float b = (float)a.GetValue();
-            return new InputData(b, 0.0f, false, false, false);
-
-		}
+            float thrust = (float)_b.GetVariable("thrust").GetValue();
+            float orientation = (float) _b.GetVariable("orientation").GetValue();
+            bool shoot = (bool)_b.GetVariable("shoot").GetValue();
+            bool dropMine = (bool) _b.GetVariable("dropMine").GetValue();
+            bool fireShockwave = (bool) _b.GetVariable("fireShockwave").GetValue();
+            return new InputData(thrust, orientation, shoot, dropMine, fireShockwave);
+        }
 	}
 
 }
