9e09e517342d3acdaf0b37e918f320834c31ec3a
Cocomineral <tintinch59@gmail.com>
2023-10-25 09:19:17 +0200

    Renamed IcarusController to Icarus

---
+++ Assets/Teams/IcarusAI/Icarus.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DoNotModify;
+using BehaviorDesigner.Runtime;
+
+namespace Icarus
+{
+
+	public class Icarus : BaseSpaceShipController
+    {
+        [SerializeField] private BehaviorTree _b;
+
+        private SpaceShipView _otherSpaceship;
+
+		public override void Initialize(SpaceShipView spaceship, GameData data)
+		{
+            base.Initialize(spaceship, data);
+            _otherSpaceship = data.GetSpaceShipForOwner(1 - spaceship.Owner);
+        }
+
+        public override InputData UpdateInput(SpaceShipView spaceship, GameData data)
+		{
+            //float thrust = 1.0f;
+            //float targetOrient = spaceship.Orientation + 90.0f;
+            //bool needShoot = AimingHelpers.CanHit(spaceship, otherSpaceship.Position, otherSpaceship.Velocity, 0.15f);
+            //return new InputData(thrust, targetOrient, needShoot, false, false);
+            SharedVariable a = _b.GetVariable("thrust");
+            float b = (float)a.GetValue();
+            return new InputData(b, 0.0f, false, false, false);
+
+		}
+	}
+
+}
