9f496f4b3c99eb54204c68f79d329144487866f9
Cocomineral <tintinch59@gmail.com>
2023-10-26 22:30:50 +0200

    [ADD] Detection of the bullet

---
+++ Assets/Teams/IcarusAI/DetectBullet.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using DoNotModify;
+
+namespace Icarus
+{
+    public class DetectBullet : Action
+    {
+        [BehaviorDesigner.Runtime.Tasks.Tooltip("The distance at which the enemy is considered close")]
+        public SharedFloat radius;
+
+        public SharedBool takeShipRadius;
+
+        private SpaceShipView _icarusShip;
+
+        public override void OnAwake()
+        {
+            _icarusShip = gameObject.GetComponent<Icarus>().IcarusShip;
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            float computeRadius = takeShipRadius.Value ? radius.Value + _icarusShip.Radius : radius.Value;
+
+            Collider2D hit = Physics2D.OverlapCircle(_icarusShip.Position, computeRadius);
+
+            if (!hit)
+            {
+                return TaskStatus.Failure;
+            }
+
+            return hit.CompareTag("Bullet") ? TaskStatus.Success : TaskStatus.Failure;
+        }
+
+    }
+
+}
