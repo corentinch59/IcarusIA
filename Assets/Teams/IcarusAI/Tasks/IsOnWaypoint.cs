aa8f9b7c52ee4dec29430193f55de93fed602273
Cocomineral <tintinch59@gmail.com>
2023-10-26 23:04:53 +0200

    [ADD] Is On Waypoint conditional task

---
+++ Assets/Teams/IcarusAI/Tasks/IsOnWaypoint.cs
@@ -0,0 +1,27 @@
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using DoNotModify;
+
+namespace Icarus
+{
+    [TaskCategory("Icarus")]
+    public class IsOnWaypoint : Conditional
+    {
+        public SharedVector2 targetPosition;
+        public SharedFloat minimalDistance;
+
+        private SpaceShipView _icarusShip;
+
+        public override void OnAwake()
+        {
+            _icarusShip = gameObject.GetComponent<Icarus>().IcarusShip;
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            float distance = (targetPosition.Value - _icarusShip.Position).sqrMagnitude;
+
+            return distance <= minimalDistance.Value * minimalDistance.Value ? TaskStatus.Success : TaskStatus.Failure;
+        }
+    }
+}
