1c9191a4ab1f4f86aee668b256174bc4de83fa55
Cocomineral <tintinch59@gmail.com>
2023-10-26 10:13:41 +0200

    [ADD] Is Enemy Hit task

---
+++ Assets/Teams/IcarusAI/IsEnemyHit.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using DoNotModify;
+
+namespace Icarus
+{
+    [TaskCategory("Icarus/Tasks")]
+    public class IsEnemyHit : Action
+    {
+        private SpaceShipView _enemyShip;
+
+        public override void OnAwake()
+        {
+            _enemyShip = gameObject.GetComponent<Icarus>().EnemyShip;
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            if (_enemyShip.HitPenaltyCountdown <= 0)
+            {
+                return TaskStatus.Success;
+            }
+
+            return TaskStatus.Failure;
+        }
+    }
+}
+
